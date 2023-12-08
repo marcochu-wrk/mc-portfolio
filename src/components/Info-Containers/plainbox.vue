afe2214a02fcfd7108e8fe57eb8f6fdd1e8c0589
marcochu-wrk <marco@malander.co.za>
2023-12-08 16:13:44 +0200

    Home and plainbox edits

--- src/components/Info-Containers/plainbox.vue
+++ src/components/Info-Containers/plainbox.vue
@@ -3,12 +3,12 @@
     <div class=" heading-text grid grid-cols-1 md:grid-cols-3 gap-6">
         
         <div class="component-style subtext md:col-span-2">Item 1</div>
-        <div class="component-style subtext">Item 2</div>
+        <div class="component-style subtext">{{ myStats.age }}</div>
         </div>
 </template>
 
 <script>
 export default{
-    props:['heading']
+    props:['heading','myStats']
 }
 </script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,15 +2,22 @@
   <div class="grid grid-cols-2 gap-4 p-8 pt-10">
     <div class="col-span-2 md:col-span-1">
       <h1 class="mb-2 heading-text text-left text-5xl">Marco Chu</h1>
-      <p class="text-lg subtext ml-1">
+      <p class="text-xl text-left text-white border-b-2 border-green-400 pb-3">
         Front-End Web Developer | Unity Developer | Graphic Designer
       </p>
-      <p class="subtext mt-8">
-        {{ welcomeMsg }}
+      <p class="subtext mt-6">
+        Welcome to my portfolio! Here I describe my hobbies, experience and general projects I do from time to time. To give you a quick
+        summary, I studied and recieved my National Senior Certificate in 2016 at Saheti. I then went on to study at the University of Witwaterstrand where
+        I received a Bachelors of Arts in Digital Arts in 2021.<br> <br>
+        Within the same year I started my career as a game 
+        developer where I worked on VR simulations for the Boiler Room. I then moved into web development in 2022 with QwidPro where I am now working as a front-end developer whilst 
+        honing my skills in the Vue.js and Laravel framework. <br><br>
+        To sum myself up, I am passionate in the technological field and am always interested in finding unique and interesting ways to improve the projects I work on.
+        I am always open to critcism and try my best to make sure that the work I put out are to the best of my capabilities.
       </p>
     </div>
 
-    <div class="col-span-2 md:col-span-1 flex justify-center items-center">
+    <div class="col-span-2 md:col-span-1 flex justify-center items-center mt-10">
       <img
         src="https://via.placeholder.com/400"
         alt="Placeholder Image"
@@ -19,7 +26,7 @@
     </div>
 
     <div class="col-span-2 mt-10">
-      <plainbox heading="Summary" />
+      <plainbox heading="About Me" :myStats="myStats" />
     </div>
 
     <div class="col-span-2">
@@ -50,7 +57,13 @@ export default {
   data(){
     return{
       homes:[],
-      welcomeMsg:"Welcome to my portfolio! Here I describe my hobbies, experience and general projects I do from time to time."
+      myStats:{
+        age: 'Age: 24',
+        birthday:'Birthday: 02 Jan 1999',
+        home:'Home Town: Hong Kong',
+        currentHome:'Residing Now: Johannesburg, South Africa',
+        languages:'Languages: English, Cantonese'
+      }
     }
   },
   mounted(){
