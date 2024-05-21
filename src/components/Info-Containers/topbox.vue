b0dce5ce2cfe6ec225619a95d0267fd9a36e363d
marcochu-wrk <119313395+marcochu-wrk@users.noreply.github.com>
2024-05-21 15:15:20 +0200

    Image Fixes

--- src/components/Info-Containers/topbox.vue
+++ src/components/Info-Containers/topbox.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-between items-center bg-gray-900  border-yellow-400 border-b-2 border-solid">
+    <div class="sticky top-0  flex justify-between items-center bg-gray-900">
         <div>{{ headingRight }}</div>
             <div class="flex items-center mr-6 mt-2 mb-2">
                 <div>{{ headingLeft1 }}</div>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,27 +1,17 @@
 <template>
-  <div class="grid grid-cols-2 gap-4 pr-8 pt-10">
+  <div class="grid grid-cols-2 gap-4 pr-8 scroll-pt-1.5">
     <div class="col-span-2 md:col-span-1">
-      <h1 class="mb-2 heading-text text-left text-5xl">Marco Chu</h1>
-      <p class="text-xl text-left text-white border-b-2 border-green-400 pb-3">
-        Front-End Web Developer | Unity Developer | Graphic Designer
-      </p>
-      <p class="subtext mt-6">
-        Welcome to my portfolio! Here I describe my hobbies, experience and general projects I do from time to time. To give you a quick
-        summary, I studied and recieved my National Senior Certificate in 2016 at Saheti. I then went on to study at the University of Witwaterstrand where
-        I received a Bachelors of Arts in Digital Arts in 2021.<br> <br>
-        Within the same year I started my career as a game 
-        developer where I worked on VR simulations for the Boiler Room. I then moved into web development in 2022 with QwidPro where I am now working as a front-end developer whilst 
-        honing my skills in the Vue.js and Laravel framework. <br><br>
-        To sum myself up, I am passionate in the technological field and am always interested in finding unique and interesting ways to improve the projects I work on.
-        I am always open to critcism and try my best to make sure that the work I put out are to the best of my capabilities.
+      <h1 class="mb-2 text-8xl text-center mt-28">Marco Chu</h1>
+      <p class="text-xl text-center text-white border-b-2 border-green-400 pb-3 mt-10">
+        Front-End Vue.js Developer | Unity Developer | UX/UI Designer
       </p>
     </div>
-    <div class="relative">
-      <div class="col-span-2 md:col-span-1 flex justify-center items-center relative">
+    <div>
+      <div class="col-span-2 md:col-span-1 flex justify-center items-center">
         <img
           src="../assets/Image-1.png"
           alt="Placeholder Image"
-          class="rounded-full border-4 border-green-500 w-3/5 mt-9 md:mt-9"
+          class="rounded-full border-4 border-green-300 w-3/5 mt-9 md:mt-9"
         />
       </div>
     </div>
