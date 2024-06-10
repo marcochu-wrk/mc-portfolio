83ea0d4fcb9a3694eaf8c0bcd26fcdb5803a94f3
marcochu-wrk <119313395+marcochu-wrk@users.noreply.github.com>
2024-06-10 22:26:24 +0200

    Heading Fixes

--- src/components/Info-Containers/equalplainbox.vue
+++ src/components/Info-Containers/equalplainbox.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="col-span-2">
-    <h2 class="text-2xl font-bold mb-2">{{ heading }}</h2>
+    <h2 class="heading-text text-left mb-2 pb-10">{{ heading }}</h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div class="component-style subtext p-4"></div>
         <div class="component-style subtext p-4"></div>
